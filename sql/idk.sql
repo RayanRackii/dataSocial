06aaae30b817a92d9eaef50d6aed7ec9b8fe6042
Rayan Khaleel Racki Abu Ali <rayan@weg.net>
2025-06-06 15:01:04 -0300

    Transfer

---
+++ sql/idk.sql
@@ -0,0 +1,16 @@
+/*
+
+Data cleaning steps
+
+1. Remove unnecessary columns
+2. Extract 
+3. Rename the columns
+
+*/
+
+SELECT 
+	NAME,
+	total_subscribers,
+	total_views,
+	total_videos
+FROM top_youtubers_2024
